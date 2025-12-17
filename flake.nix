1003c098a6d87bf918a4304d6bb7f5c146332e64
Ujaan Das <ujaandas03@gmail.com>
2025-12-17 12:38:41 +0000

    fix flake for v2

--- flake.nix
+++ flake.nix
@@ -24,20 +24,9 @@
           vendorHash = "sha256-gM37SLXNi4uY3uetmagNarbUvaFapQciajrguWVSd34=";
 
           nativeBuildInputs = with pkgs; [ tailwindcss ];
-
-          buildPhase = ''
-            tailwindcss \
-              --input ./public/styles.css \
-              --output $out/public/styles.css \
-              --minify \
-              --config ./tailwind.config.js
-
-            mkdir -p $out/bin
-            go build -o $out/bin/oojsite .
-          '';
         };
       in
-      rec {
+      {
         devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             go
@@ -49,68 +38,6 @@
         };
 
         defaultPackage = oojsite;
-        defaultApp = apps.oojsite-run;
-
-        apps = {
-          oojsite-run = {
-            type = "app";
-            program = "${pkgs.writeShellScriptBin "oojsite-run" ''
-              outdir="./out"
-              rm -rf $outdir
-              echo "Generating site into $outdir"
-
-              ${oojsite}/bin/oojsite --out "$outdir"
-
-              mkdir -p "$outdir/public"
-              cp -r ${oojsite}/public/. "$outdir/public/"
-
-              echo "View site at file://$outdir/index.html"
-            ''}/bin/oojsite-run";
-          };
-
-          watch-rb = {
-            type = "app";
-            program = "${pkgs.writeShellScriptBin "oojsite-watch" ''
-              echo "Watching for changes and regenerating site..."
-
-              watchexec \
-                --restart \
-                --clear \
-                --watch ./public \
-                --watch ./templates \
-                --watch ./site \
-                --watch ./main.go \
-                --exts css,html,go,md \
-                -- nix run
-            ''}/bin/oojsite-watch";
-          };
-
-          watch-no-rb = {
-            type = "app";
-            program = "${pkgs.writeShellScriptBin "oojsite-watch" ''
-              echo "Watching site templates and regenerating..."
-
-              watchexec \
-                --restart \
-                --clear \
-                --watch ./templates \
-                --watch ./site \
-                --watch ./public \
-                --exts html,go,md,css \
-                -- 'tmpdir=$(mktemp -d);
-                  go run . --out $tmpdir || exit 1;
-
-                  tailwindcss \
-                    --input ./public/styles.css \
-                    --output "$tmpdir/public/styles.css" \
-                    --minify \
-                    --config ./tailwind.config.js || exit 1;
-
-                  cp -r $tmpdir/public/. ./out/public/
-                '
-            ''}/bin/oojsite-watch";
-          };
-        };
       }
     );
 }
