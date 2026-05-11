c2b6694ca2d489965caf60aba79632ca54175a9c
Ujaan Das <ujaandas03@gmail.com>
2026-05-11 12:57:44 +0000

    add allDir option which prepends other option dirs

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,9 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 type Config struct {
+	AllDir       string
 	OutDir       string
 	PageDir      string
 	PostDir      string
@@ -20,6 +22,7 @@ type Config struct {
 func Parse() (*Config, error) {
 	cfg := &Config{}
 
+	flag.StringVar(&cfg.AllDir, "allDir", "", "Base directory to prepend to other paths (site, posts, templates, components, static)")
 	flag.StringVar(&cfg.OutDir, "outDir", "out", "Path to generate site in")
 	flag.StringVar(&cfg.PageDir, "pageDir", "site", "Path to pages folder")
 	flag.StringVar(&cfg.PostDir, "postDir", "posts", "Path to posts folder")
@@ -30,6 +33,25 @@ func Parse() (*Config, error) {
 	flag.BoolVar(&cfg.Dev, "dev", false, "Start development server")
 
 	flag.Parse()
+
+	// Apply allDir prefix to paths that still have their default values
+	if cfg.AllDir != "" {
+		if cfg.PageDir == "site" {
+			cfg.PageDir = filepath.Join(cfg.AllDir, "site")
+		}
+		if cfg.PostDir == "posts" {
+			cfg.PostDir = filepath.Join(cfg.AllDir, "posts")
+		}
+		if cfg.StaticDir == "static" {
+			cfg.StaticDir = filepath.Join(cfg.AllDir, "static")
+		}
+		if cfg.TemplateDir == "templates" {
+			cfg.TemplateDir = filepath.Join(cfg.AllDir, "templates")
+		}
+		if cfg.ComponentDir == "components" {
+			cfg.ComponentDir = filepath.Join(cfg.AllDir, "components")
+		}
+	}
 
 	if err := validateDirs(cfg); err != nil {
 		return nil, err
